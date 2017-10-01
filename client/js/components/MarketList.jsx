a283159a0d94d7cf5fe9f5b0667663003e7efb57
Arturo <arturo.p3rez@gmail.com>
2017-09-30 22:11:46 -0500

    added functionality to table

--- client/js/components/MarketList.jsx
+++ client/js/components/MarketList.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import MarketItem from './MarketItem';
-import {ListGroup, ListGroupItem} from 'reactstrap';
+import {Table} from 'reactstrap';
+import BootstrapTable from 'reactjs-bootstrap-table';
 
-export default class Example extends React.Component {
+export default class MarketList extends React.Component {
+
+    //sort the table
+    onSort(col, dir) {
+        this.props.sort(col, dir);
+    }
+
+    //definition of table columns
+    columns = [
+        { name: 'col1', display: 'Market A' },
+        { name: 'col2', display: 'Market B' },
+        { name: 'spread', display: 'Arbitrage Spread' }
+    ]
+
+    //generate the table
     render() {
-        const marketItems = marketArr.map(market => <MarketItem market = {market} />);
         return (
-        <ListGroup>
-            {marketItems}
-        </ListGroup>
+        <BootstrapTable data = { marketsArr }>
+            onSort = {this.onSort} 
+            columns = {columns}
+            </BootstrapTable>
         );
     }
+
+    //https://github.com/raythree/reactjs-bootstrap-table-demo/blob/8618565b5221479423fe55995626385018f3c129/src/components/PageOne.js
+    //http://demo.ray3.io/bootstrap-table/
+    //https://www.npmjs.com/package/reactjs-bootstrap-table
+    
 }
